70e1686020769db1abdafdf23edb93eb2a1d577a
BlerinaP <blerina1.p@gmail.com>
2019-11-19 10:55:59 +0100

    Making main menu responsive

--- header.php
+++ header.php
@@ -8,16 +8,28 @@
 <body>
 
 <header>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-               <img src="<?php echo get_stylesheet_directory_uri() ?>/img/logo.png" class="img-responsive">
-            </div>
-            <div class="col-sm-8">
-                <?php wp_nav_menu(array(
-                   'theme_location' => 'main_menu'
-                ));?>
+    <nav>
+        <div class="container">
+            <div class="row">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                   <img src="<?php echo get_stylesheet_directory_uri() ?>/img/logo.png" class="img-responsive">
+                </div>
+
+                <div class="navbar-right">
+                    <?php wp_nav_menu(array(
+                       'theme_location' => 'main_menu',
+                        'container_id' => 'navbar',
+                        'container_class' => 'collapse navbar-collapse',
+                        'menu_class' => 'nav navbar-nav navbar-right'
+                    ));?>
+                </div>
             </div>
         </div>
-    </div>
+    </nav>
 </header>
